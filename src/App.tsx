12d421b02e33df11c186cda0cfa1a8527808acc6
Pagebabe <pagebabe63@gmail.com>
2026-07-05 10:34:14 +0200

    Support existing keyframe placement route

--- src/App.tsx
+++ src/App.tsx
@@ -58,6 +58,7 @@ export default function App() {
       case '/prompt-workbench':
         return <PromptWorkbench />;
       case '/frame-inbox':
+      case '/keyframe-placement':
         return <FrameInbox />;
       case '/story-bible':
         return <StoryBible />;
